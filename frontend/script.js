c5ee210f059391bb5e9bdfedd1650f53462dd0f1
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-10 23:09:36 +0545

    Implement form submission

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+let form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  let username = document.querySelector("#username").value;
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+
+  try {
+    const response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify({ username, email, password }),
+    });
+    const data = await response.json();
+    console.log("Data sent to server!", data);
+  } catch (error) {
+    console.log("Error fetching data::", error);
+  }
+});
