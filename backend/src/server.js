55c3fdcc055d65be623711e7db25af5aed304520
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-10 23:10:47 +0545

    setup  CORS middleware and update response

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config({
-    path:"./.env"
+  path: "./.env",
 });
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Working!!!");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.post("/signup", (req, res) => {
+  res.json("Working!!!");
 });
 
 app.listen(process.env.PORT, () => {
