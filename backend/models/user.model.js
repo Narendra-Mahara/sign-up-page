98f04078770a54b78140740c9c79a5128fabdd48
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-11 10:38:51 +0545

    create user model with schema for username, email, and password

---
+++ backend/models/user.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
