32c340b148e47cf33b06f05f8ba551d97534128c
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-11 09:49:38 +0545

    add middleware and connect to db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import router from "../routes/user.route.js";
+import connectDB from "../db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const app = express();
+connectDB(); // connect to the database
 
 app.use(
   cors({
@@ -13,9 +16,9 @@ app.use(
   })
 );
 
-app.post("/signup", (req, res) => {
-  res.json("Working!!!");
-});
+app.use("/api/v1/user", router);
+app.use(express.json()); //to parse JSON bodies
+app.use(express.urlencoded({ limit: "16kb", extended: true })); //to parse urlencoded bodies for form data
 
 app.listen(process.env.PORT, () => {
   console.log(`server is running on http://localhost:${process.env.PORT}/`);
