f30210615cdddd72570aed705978c220dfdaad88
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-12 09:26:15 +0545

    feat: add login route to user routes

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import signUpUser from "../controller/user.controller.js";
+import { signUpUser, loginUser } from "../controller/user.controller.js";
 
 const router = Router();
 router.route("/signup").post(signUpUser); // /api/v1/user/signup yo ma req aaye paxi signupuser controller ma jane
+router.route("/login").post(loginUser); // /api/v1/user/login yo ma req aaye paxi signupuser controller ma jane
 
 export default router;
